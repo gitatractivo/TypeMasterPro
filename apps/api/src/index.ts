626305ed1f515e81d01910e9fc1328b6f460669a
Gitanshu Talwar <gitanshutalwar@gmail.com>
2025-02-22 18:37:32 +0530

    final example

---
+++ apps/api/src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+
+
+const port = process.env.PORT || 8000;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+
